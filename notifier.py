22baf7fce4c0305263e2c7de01198476b362d8c3
KalanaBimsara <kalanabimsara8@gmail.com>
2026-04-20 15:22:06 +0530

    notiication from backend

---
+++ notifier.py
@@ -0,0 +1,13 @@
+"""
+notifier.py - Generates notifications based on food freshness detections.
+"""
+
+import logging
+
+log = logging.getLogger(__name__)
+
+
+
+
+    log.info(f"Generated {len(notifications)} notification(s).")
+    return notifications
